3ea36868c98fcaeb5fa70da704a678b9e1570efd
Daniel Atkinson <atkinson.daniel7@gmail.com>
2025-04-29 20:16:37 -0600

    Insert when root is null

--- include/red_black_tree.hpp
+++ include/red_black_tree.hpp
@@ -15,14 +15,35 @@ enum Color
 template <typename T>
 class RedBlackTree
 {
-private:
+public:
   struct Node
   {
     T data;
     Color color;
     Node *left;
     Node *right;
+    Node(T value) : data(value), color(RED), left(nullptr), right(nullptr) {};
   };
+
+  RedBlackTree() : root(nullptr) {};
+
+  // Test if data was set correctly
+  Node &get_root()
+  {
+    return *root;
+  }
+  // TODO: Modify later to avoid copy of data
+  void insert(T data)
+  {
+    if (root == nullptr)
+    {
+      root = new Node(data);
+      root->color = BLACK;
+    }
+  }
+
+private:
+  Node *root;
 };
 
 #endif // RED_BLACK_TREE_CPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
-#include <dense_matrix.hpp>
+#include <red_black_tree.hpp>
 
 int main()
 {
   DenseMatrix dense_matrix(5, 4);
   dense_matrix(1, 1, 9.0);
-  std::cout << dense_matrix << std::endl;
+  RedBlackTree<DenseMatrix> rbt;
+  rbt.insert(dense_matrix);
+  // Test if root is set correctly
+  std::cout << rbt.get_root().data << std::endl;
   return 0;
 }
