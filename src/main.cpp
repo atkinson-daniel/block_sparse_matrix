1310ba91491b7e6d583a1976d0d8c9b6f3504f0c
Daniel Atkinson <atkinson.daniel7@gmail.com>
2025-05-05 16:08:11 -0700

    Update Main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,36 @@ int main()
   DenseMatrix dense_matrix2(4, 4);
   DenseMatrix dense_matrix3(4, 4);
   DenseMatrix dense_matrix4(4, 4);
+
   dense_matrix1(0, 0, 1);
+  dense_matrix1(1, 1, 1);
+  dense_matrix1(2, 2, 1);
+  dense_matrix1(3, 3, 1);
+  dense_matrix1(0, 1, 1);
+
   dense_matrix2(0, 0, 2);
-  dense_matrix3(2, 0, 3);
+  dense_matrix2(1, 1, 2);
+  dense_matrix2(2, 2, 2);
+  dense_matrix2(3, 3, 2);
+  dense_matrix2(0, 1, 2);
+
+  dense_matrix3(0, 0, 3);
+  dense_matrix3(1, 1, 3);
+  dense_matrix3(2, 2, 3);
+  dense_matrix3(3, 3, 3);
+  dense_matrix3(0, 1, 3);
+
   dense_matrix4(0, 0, 4);
+  dense_matrix4(1, 1, 4);
+  dense_matrix4(2, 2, 4);
+  dense_matrix4(3, 3, 4);
+  dense_matrix4(0, 1, 4);
 
   bsm[0, 0] = dense_matrix1;
   bsm[1, 1] = dense_matrix2;
   bsm[1, 0] = dense_matrix3;
   bsm[0, 1] = dense_matrix4;
+
   std::cout << bsm << std::endl;
 
   bsm.print_tree();
@@ -27,22 +48,6 @@ int main()
   std::cout << bsm << std::endl;
 
   bsm.print_tree();
-
-  /*
-    RedBlackTree<DenseMatrix> rbt(10);
-    rbt.insert(dense_matrix1, 0, 0);
-    rbt.insert(dense_matrix2, 1, 0);
-    rbt.insert(dense_matrix3, 2, 0);
-    rbt.insert(dense_matrix4, 1, 1);
-
-    std::cout << rbt.size() << std::endl;
-    rbt.print_tree();
-
-    rbt.remove(2, 0);
-    std::cout << rbt.size() << std::endl;
-
-    rbt.print_tree();
-  */
 
   return 0;
 }
